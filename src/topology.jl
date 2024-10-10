0efb00aee9aec9143c048e7d7046a201103ac43c
Benoit Pasquier <briochemc@gmail.com>
2024-10-10 22:19:07 +1100

    Simplify topology code a tiny bit

--- src/topology.jl
+++ src/topology.jl
@@ -35,17 +35,17 @@ end
 
 # Default behavior for grids (assumed bipolar)
 # wrap around in the i direction (longitude)
-i₊₁(C, g::AbstractGrid) = C.I[1] < g.nx ? C .+ CartesianIndex(1, 0, 0) : CartesianIndex(1, C.I[2], C.I[3])
-i₋₁(C, g::AbstractGrid) = C.I[1] > 1 ? C .- CartesianIndex(1, 0, 0) : CartesianIndex(g.nx, C.I[2], C.I[3])
+i₊₁(C, g::AbstractGrid) = C.I[1] < g.nx ? C + CartesianIndex(1, 0, 0) : CartesianIndex(1, C.I[2], C.I[3])
+i₋₁(C, g::AbstractGrid) = C.I[1] > 1 ? C + CartesianIndex(-1, 0, 0) : CartesianIndex(g.nx, C.I[2], C.I[3])
 # No connection by default in the j direction (latitude)
-j₊₁(C, g::AbstractGrid) = C.I[2] < g.ny ? C .+ CartesianIndex(0, 1, 0) : nothing
-j₋₁(C, ::AbstractGrid) = C.I[2] > 1 ? C .- CartesianIndex(0, 1, 0) : nothing
+j₊₁(C, g::AbstractGrid) = C.I[2] < g.ny ? C + CartesianIndex(0, 1, 0) : nothing
+j₋₁(C, ::AbstractGrid) = C.I[2] > 1 ? C + CartesianIndex(0, -1, 0) : nothing
 # No connection by default in the k direction (depth)
-k₊₁(C, g::AbstractGrid) = C.I[3] < g.nz ? C .+ CartesianIndex(0, 0, 1) : nothing
-k₋₁(C, ::AbstractGrid) = C.I[3] > 1 ? C .- CartesianIndex(0, 0, 1) : nothing
+k₊₁(C, g::AbstractGrid) = C.I[3] < g.nz ? C + CartesianIndex(0, 0, 1) : nothing
+k₋₁(C, ::AbstractGrid) = C.I[3] > 1 ? C + CartesianIndex(0, 0, -1) : nothing
 
-# Special behavior for tripolar grids
-j₊₁(C, g::TripolarGrid) = C.I[2] < g.ny ? C .+ CartesianIndex(0, 1, 0) : CartesianIndex(mod1(g.nx - C.I[1] + 1, g.nx), g.ny, C.I[3])
+# Special behavior for tripolar grids where the seam connects the top and "folds" it around
+j₊₁(C, g::TripolarGrid) = C.I[2] < g.ny ? C + CartesianIndex(0, 1, 0) : CartesianIndex(mod1(g.nx - C.I[1] + 1, g.nx), g.ny, C.I[3])
 
 # error for unknown grids
 i₊₁(C, ::UnknownGrid) = error("Unknown grid type")
